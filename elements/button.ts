9d4d8e81dc394972eb1fa41f608db74e867eeb23
AnisovichMatvey <m.anisovich@qa-academy.by>
2021-04-15 00:36:50 +0300

    Added doubleClick method to button element

--- elements/button.ts
+++ elements/button.ts
@@ -5,4 +5,22 @@ export class Button extends Element {
     constructor(selector: string, name: string, options?: ElementOptions) {
         super(selector, name, options);
     }
+
+    /**
+     * Click the element
+     */
+    doubleClick() {
+        this._waitForExist();
+        this._doubleClick();
+    }
+
+    protected _doubleClick(options?: { element: Cypress.Chainable<JQuery<HTMLElement>> }) {
+        this._waitForLogWrapper(() => {
+            if (options && options.element) {
+                options.element.dblclick();
+            } else {
+                this.get$().dblclick();
+            }
+        });
+    }
 }
